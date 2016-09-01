096902728f1bf3fce2b240c84aff8528c68096d8
Taichi Koido <taichi1009@gmail.com>
2016-09-01 12:30:11 +0900

    clean up bugs

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   devise_for :users
   root "prototypes#index"
-=======
-  root 'prototypes#index'
   resources :prototypes
   # get  '/prototypes/index'  =>    'prototypes#index'
   # get  '/prototypes/new'    =>    'prototypes#new'
   # get  '/prototypes/show'   =>    'prototypes#show'
->>>>>>> master
 end
