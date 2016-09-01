47ebd656f91110cdc4a7e7be9db576cbdb036a91
Taichi Koido <taichi1009@gmail.com>
2016-09-01 14:41:01 +0900

    add association between user and prototypes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user, only: [:show, :edit, :update]
+  def show
+    @protypes = @user.prototypes.all
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+  def update_params
+    params.require(:user).permit(:nickname, :avatar, :memeber_of, :profile, :works)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
